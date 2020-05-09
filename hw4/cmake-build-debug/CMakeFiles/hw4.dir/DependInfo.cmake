aeba915bb89fe8170eb7cb3a017178d786f39613
Tavis991 <45292394+Tavis991@users.noreply.github.com>
2020-05-09 09:23:04 +0300

    Add files via upload

---
+++ hw4/cmake-build-debug/CMakeFiles/hw4.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/tuli/CLionProjects/hw4/League.c" "/home/tuli/CLionProjects/hw4/cmake-build-debug/CMakeFiles/hw4.dir/League.c.o"
+  "/home/tuli/CLionProjects/hw4/Match.c" "/home/tuli/CLionProjects/hw4/cmake-build-debug/CMakeFiles/hw4.dir/Match.c.o"
+  "/home/tuli/CLionProjects/hw4/Team.c" "/home/tuli/CLionProjects/hw4/cmake-build-debug/CMakeFiles/hw4.dir/Team.c.o"
+  "/home/tuli/CLionProjects/hw4/main.c" "/home/tuli/CLionProjects/hw4/cmake-build-debug/CMakeFiles/hw4.dir/main.c.o"
+  "/home/tuli/CLionProjects/hw4/sort_league.c" "/home/tuli/CLionProjects/hw4/cmake-build-debug/CMakeFiles/hw4.dir/sort_league.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ hw4/cmake-build-debug/CMakeFiles/hw4.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw4.dir/League.c.o"
+  "CMakeFiles/hw4.dir/Match.c.o"
+  "CMakeFiles/hw4.dir/Team.c.o"
+  "CMakeFiles/hw4.dir/main.c.o"
+  "CMakeFiles/hw4.dir/sort_league.c.o"
+  "hw4"
+  "hw4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hw4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
